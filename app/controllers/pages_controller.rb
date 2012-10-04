e90e4af821ea995f34042996b8b1ce092fd61196
pti4ka <khalimovaguliya@gmail.com>
2012-10-04 15:22:51 +0400

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,15 @@
 class PagesController < ApplicationController
+
   def home
+    @title = "Home" #Переменная экземпляра
+	#В Рельсах связывают ДЕЙСТВИЕ - с ПРЕДСТАВЛЕНИЕМ
   end
 
   def contact
+    @title = "Contact"
+  end
+
+  def about
+    @title = "About"
   end
 end
